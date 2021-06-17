a98f9a9226511f439744b8cd082c909f22c515ea
huyphuoc206 <huyphuoc206@gmail.com>
2021-06-18 06:09:49 +0700

    Config database heroku

--- src/utils/DBUtils.java
+++ src/utils/DBUtils.java
@@ -10,9 +10,9 @@ public class DBUtils {
 	public static Connection getConnection() {
 		try {
 			if (connection == null || connection.isClosed()) {
-				String url = "jdbc:postgresql://localhost/coffee-shop";
-				String user = "postgres";
-				String password = "php2006";
+				String url = "jdbc:postgresql://ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d6du86ba1ppihr";
+				String user = "oaansujdhqtvrr";
+				String password = "aac90239b01151e3f8b0a624249c23248c72422e2d1508da76338debb2bca6a0";
 				connection = DriverManager.getConnection(url, user, password);
 			}
 		} catch (SQLException throwables) {
