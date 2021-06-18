e1cd49e75d9ce9f246fe4c245f99fb5bb6d97558
huyphuoc206 <huyphuoc206@gmail.com>
2021-06-18 08:43:05 +0700

    class for name

--- src/utils/DBUtils.java
+++ src/utils/DBUtils.java
@@ -10,6 +10,7 @@ public class DBUtils {
 	public static Connection getConnection() {
 		try {
 			if (connection == null || connection.isClosed()) {
+				Class.forName("org.postgresql.Driver");
 				String url = "jdbc:postgresql://ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d6du86ba1ppihr";
 				String user = "oaansujdhqtvrr";
 				String password = "aac90239b01151e3f8b0a624249c23248c72422e2d1508da76338debb2bca6a0";
@@ -17,6 +18,8 @@ public class DBUtils {
 			}
 		} catch (SQLException throwables) {
 			throwables.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 		return connection;
 	}
